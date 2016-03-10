510bad953bdc3427bc3ef24c6ac7513bc5e9d0ab
Naveed Fida <naveedfida01@gmail.com>
2016-03-10 12:50:07 -0800

    passed test_suit for grade_school

--- grade_school/grade_school.rb
+++ grade_school/grade_school.rb
@@ -1,14 +1,23 @@
+require 'pry'
 class School
   def initialize
     @students = {}
   end
 
-  def add(student, idx)
-    @students[idx] ||= []
-    @students[idx] << student
+  def add(name, grade)
+    @students[grade] ||= []
+    @students[grade] << name
   end
 
   def to_h
-    @students
+    sorted = {}
+    sorted_by_grade = @students.sort_by {|grd, names| grd}
+    sorted_by_grade.each { |grd, names| names.sort! }.each {|grd, name| sorted[grd] = name}
+    sorted
+  end
+
+  def grade(grade)
+    return [] if !@students[grade]
+    @students[grade]
   end
 end
--- grade_school/grade_school_test.rb
+++ grade_school/grade_school_test.rb
@@ -23,7 +23,6 @@ class SchoolTest < Minitest::Test
   end
 
   def test_add_more_students_in_same_class
-    skip
     school.add('Blair', 2)
     school.add('James', 2)
     school.add('Paul', 2)
@@ -31,14 +30,12 @@ class SchoolTest < Minitest::Test
   end
 
   def test_add_students_to_different_grades
-    skip
     school.add('Chelsea', 3)
     school.add('Logan', 7)
     assert_equal({ 3 => ['Chelsea'], 7 => ['Logan'] }, school.to_h)
   end
 
   def test_get_students_in_a_grade
-    skip
     school.add('Bradley', 5)
     school.add('Franklin', 5)
     school.add('Jeff', 1)
@@ -46,12 +43,10 @@ class SchoolTest < Minitest::Test
   end
 
   def test_get_students_in_a_non_existant_grade
-    skip
     assert_equal [], school.grade(1)
   end
 
   def test_sort_school # rubocop:disable Metrics/MethodLength
-    skip
     [
       ['Jennifer', 4], ['Kareem', 6],
       ['Christopher', 4], ['Kyle', 3]
